10bc68b60d64cf6bdac483c39d2f94298cd0cb69
kfokusd <93218564+kfokusd@users.noreply.github.com>
2021-11-03 12:57:00 -0700

    Add files via upload

---
+++ quiz6.py
@@ -0,0 +1,7 @@
+
+# Function: alphabet()
+# - Reads the filename, and count the occurrence of 'A'-'Z' and 'a'-'z'
+# - Returns a 2-items tuple.  1st tuple item is a dictionary with 'A' to 'Z' as keys and the 
+#   corresponding value is the occurrence of the corresponding alphabet.  
+#   2nd tuple item is a dictionary for 'a' to 'z' as keys and the corresponding value is the occurrence of the 
+#   corresponding alphabet
---
+++ quiz6_main.py
@@ -0,0 +1,29 @@
+import quiz6 as q6
+import matplotlib.pyplot as pp
+
+
+q6.alphabet("quiz6_article.txt")      # Need to modify this code
+
+## Show the Upper Case ('A' - 'Z' occurrences)
+#pp.bar( @FIXME )       # Need to modify this code, and uncomment it
+pp.show()
+
+## Show the Lower Case ('a' - 'z' occurrences)
+#pp.bar( @FIXME )       # Need to modify this code, and uncomment it
+pp.show()
+
+
+## Output the Upper Case Dictionary
+#print( @FIXME )         # Need to modify this code, and uncomment it
+print()
+## Output the Lower Case Dictionary
+#print( @FIXME )         # Need to modify this code, and uncomment it
+
+
+#Expected Output:
+#{'A': 1, 'B': 1, 'C': 2, 'D': 1, 'E': 1, 'F': 0, 'G': 2, 'H': 1, 'I': 4, 'J': 2, 'K': 0, 'L': 4, 'M': 2, 
+#'N': 2, 'O': 1, 'P': 18, 'Q': 1, 'R': 1, 'S': 2, 'T': 6, 'U': 2, 'V': 0, 'W': 0, 'X': 1, 'Y': 0, 'Z': 0}
+#
+#{'a': 205, 'b': 44, 'c': 79, 'd': 90, 'e': 242, 'f': 49, 'g': 50, 'h': 71, 'i': 144, 'j': 7, 'k': 13, 
+# 'l': 86, 'm': 71, 'n': 178, 'o': 168, 'p': 55, 'q': 0, 'r': 142, 's': 147, 't': 199, 'u': 46, 'v': 18, 
+# 'w': 21, 'x': 9, 'y': 52, 'z': 1}
